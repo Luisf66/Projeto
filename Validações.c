7c27c241ecdb9e96751d10476a92e954ee5ef050
Luisf66 <Luisf311220@gmail.com>
2022-10-16 14:39:26 -0300

    validação cpf

--- Validações.c
+++ Validações.c
@@ -80,18 +80,32 @@ char data(char dd, char mm, char aaaa){
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
 // CPF
+
+char CPF(char *cpf,char resultado,char resultado_dois){
+    char *cpf[14];
+    char resultado[4];
+    char resultado_dois[4];
+    resultado = (cpf[0]*10) + (cpf[1]*9) + (cpf[2]*8) + (cpf[4]*7) + (cpf[5]*6) + (cpf[6]*5) + (cpf[8]*4) + (cpf[9]*3) + (cpf[10]*2); 
+    resultado = (resultado * 10) % 11;
+    if (resultado == 10){
+        resultado = 0;
+    }
+    if (resultado == cpf[12]){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    resultado_dois = (cpf[0]*11) + (cpf[1]*10) + (cpf[2]*9) + (cpf[4]*8) + (cpf[5]*7) + (cpf[6]*6) + (cpf[8]*5) + (cpf[9]*4) + (cpf[10]*3) + (cpf[12]*2);
+    resultado_dois = (resultado_dois * 10) % 11;
+    if (resultado_dois == cpf[13]){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
 
 // CNPJ
 
