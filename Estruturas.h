7e913499724277abc9b4b3a94386373ae21dcbbc
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2022-11-19 11:28:26 -0300

    Update Estruturas.h

--- Estruturas.h
+++ Estruturas.h
@@ -4,13 +4,13 @@
 typedef struct paciente Paciente;
 struct paciente{
     char nome[40];
-    char cpf[15];
-    char celular[15];
-    char dd[4];
-    char mm[4];
-    char aaaa[6];
-    char email[50];
-    char tipo[5];
+    char cpf[16];
+    char celular[16];
+    char dd[5];
+    char mm[5];
+    char aaaa[7];
+    char email[51];
+    char tipo[6];
     char status;
 };
 
