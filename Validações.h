6dac0c8245a2c207200950ad7a6c137015149292
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2022-12-05 19:25:32 -0300

    Delete Validações.h

--- Validações.h
+++
@@ -1,8 +0,0 @@
-
-char bissexto(char aaaa);
-
-char data(char dd, char mm, char aaaa);
-
-char CPF(char *cpf,int resultado,int resultado_dois);
-
-char CNPJ(char *cnpj,char resultado, char resto,char resultado_dois, char resto_dois);
