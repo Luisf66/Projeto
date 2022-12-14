8c0871c71d3641c81abc9ea1e0f9a89b08d34ff5
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2022-12-14 18:08:02 -0300

    Update Paciente.h

--- Paciente.h
+++ Paciente.h
@@ -13,3 +13,13 @@ void buscarcliente (void);
 void deletarcliente(void);
 
 void editarcliente(void);
+
+int convertedia(char* dd);
+
+int convertemes (char* mm);
+
+int converteano (char* aaaa);
+
+int validacao_data (int dia,int mes, int ano);
+
+int validacao_cpf (char* cpf);
